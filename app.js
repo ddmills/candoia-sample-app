4d8606a55de7f914d77a4ce399ffbc9840470d22
Dalton Mills <dmills@sourceallies.com>
2016-01-24 16:46:14 -0600

    updated boa query

--- app.js
+++ app.js
@@ -2,19 +2,7 @@ console.log('hello world!');
 
 api.store.put("mySecretKey", 6000);
 
-
-var results = api.boa.exec(
-    "# How many committers are in each project?" +
-    "p: Project = input;" +
-    "counts: output sum[string] of int;" +
-    "committers: map[string] of bool;" +
-    "visit(p, visitor {" +
-    "    before node: Revision ->" +
-    "        if (!haskey(committers, node.committer.username)) {" +
-    "            committers[node.committer.username] = true;" +
-    "            counts[p.id] << 1;" +
-    "        }" +
-    "}"
-);
+// this line will take some time to execute
+var results = api.boa.exec("my-boa-script.boa");
 
 console.log(results);
